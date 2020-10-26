1d6191efa9a8bbd12caafbd21fe0a0960a7169dc
shurnew <vmon07@icloud.com>
2020-10-26 15:56:33 +0900

    Updates

---
+++ precache-manifest.d5ee19a68d2e7d989fc9e4802897c64e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adef00a1b8c258ea92eb7c780ade9c0",
+    "url": "/react-deploy-test/index.html"
+  },
+  {
+    "revision": "83628a245a49046a84c4",
+    "url": "/react-deploy-test/static/css/main.7cab6f64.chunk.css"
+  },
+  {
+    "revision": "b5436bdc2229bd9f98c6",
+    "url": "/react-deploy-test/static/js/2.956cd280.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy-test/static/js/2.956cd280.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83628a245a49046a84c4",
+    "url": "/react-deploy-test/static/js/main.605303b6.chunk.js"
+  },
+  {
+    "revision": "e42619e8f05aab1be1cf",
+    "url": "/react-deploy-test/static/js/runtime-main.a76fde35.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-deploy-test/static/media/logo.ee7cd8ed.svg"
+  }
+]);
